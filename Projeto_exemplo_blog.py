3d1ba63354dec78e4ca4e541f2e1c6a858ba53cd
Bruno Freitas <97988515+bruno-freitas-pro@users.noreply.github.com>
2023-01-24 23:37:33 -0300

    Update Projeto_exemplo_blog.py

--- Projeto_exemplo_blog.py
+++ Projeto_exemplo_blog.py
@@ -1,11 +1,13 @@
-import glob                             #Ajuda a abrir os arquivos
-import pandas as pd                     #Usado para várias tarefas
-import xml.etree.ElementTree as ET      #Usado para tratar XML
-from datetime import datetime           #Usado para marcar o horário no log
+import glob                         #Ajuda a abrir os arquivos
+import pandas as pd                 #Usado para várias tarefas
+import xml.etree.ElementTree as ET  #Usado para tratar XML
+from datetime import datetime       #Usado para marcar o horário no log
 
-temp = "temp.tmp"                       #Arquivo temporário
-arquivo_log = "log.txt"                 #Registro dos fatos
-destino = "dados_transformados.csv"     #Destino na etapa de carregamento (load)
+temp = "temp.tmp" #Arquivo temporário
+arquivo_log = "log.txt" #Registro dos fatos
+destino = "dados_transformados.csv" #Destino na etapa de carregamento (load)
+
+###### ETAPA DE EXTRAÇÃO ######
 
 def extrair_csv(arquivo_base):
     dataframe = pd.read_csv(arquivo_base)
@@ -31,28 +33,29 @@ def extrair_xml(arquivo_base):
 def extract():
     #Criar um dataframe vazio pra receber os dados
     dados_extraidos = pd.DataFrame(columns=['nome','altura','peso'])
-    
+     
     #Processar os arquivos CSV
     for csv in glob.glob("*.csv"):
         dados_extraidos = pd.concat([dados_extraidos, extrair_csv(csv)], ignore_index=True)
-    
+     
     #Processar os arquivos JSON
     for json in glob.glob("*.json"):
         dados_extraidos = pd.concat([dados_extraidos, extrair_json(json)], ignore_index=True)
-    
+     
     #Processar os arquivos XML
     for xml in glob.glob("*.xml"):
         dados_extraidos = pd.concat([dados_extraidos, extrair_xml(xml)], ignore_index=True)
-    
+     
     return dados_extraidos
 
+###### ETAPA DE TRANSFORMAÇÃO ######
 def transform(dados):
         #Converter polegadas pra metros
         #Converter os dados pra 'float'
         dados.altura = dados.altura.astype(float)
         #Arredondar pra duas casas decimais
         dados['altura'] = round(dados.altura * 0.0254, 2)
-        
+         
         #Converter libras pra quilos
         #Converter os dados pra 'float'
         dados.peso = dados.peso.astype(float)
@@ -60,19 +63,21 @@ def transform(dados):
         dados['peso'] = round(dados.peso * 0.45359237, 2)
         return dados
 
+###### ETAPA DE CARREGAMENTO ######
 def load(destino, dados_exportados):
     dados_exportados.to_csv(destino)
 
+###### REGISTRO NO LOG ######
 def log(mensagem):
     #Definir o formato de data-hora
     formato_data_hora = '%Y-%m-%d-%H:%M:%S.%f'
-    
+     
     #Obter a data e hora atuais
     agora = datetime.now()
-    
+     
     #Converter a data e a hora para o formato desejado
     data_hora = agora.strftime(formato_data_hora)
-    
+     
     #Gravar as informações no log
     with open(arquivo_log,"a") as logfile:
         logfile.write(data_hora + ',' + mensagem + '\n')
@@ -81,16 +86,16 @@ log("Processo de ETL iniciado!")
 
 log("Extração iniciada!")
 dados_extraidos = extract()
-log("Fim da extração")
 print('dados extraídos:\n', dados_extraidos, '\n')
+log("Fim da extração")
 
 log("Transformação iniciada!")
 dados_transformados = transform(dados_extraidos)
-log("Transformação finalizada!")
 print('dados transformados: \n', dados_transformados, '\n')
+log("Transformação finalizada!")
 
 log("Carregamento iniciado!")
 load(destino, dados_transformados)
 log("Carregamento finalizado!")
 
-log(Processo ETL finalizado!)
+log("Processo ETL finalizado!")
